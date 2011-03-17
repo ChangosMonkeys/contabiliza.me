4fa9f4dadfd7c1571ed900c7361283386c8c25b9
José Uribe <jauc27@gmail.com>
2011-03-17 10:47:37 -0600

    first commit

---
+++ contabilizame/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contabilizame::Application.config.secret_token = 'a8ea488d413aa610aa5fe3fdf51e21f8da7585d42c0c3b37bd766fb015c2f7dd5b493753f092debba6d0906a68f1bb8fb077640fe738f15abcd83dde3ec202bc'
